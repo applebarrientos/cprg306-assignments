a236681d4f8f91b2dc6b9346d4bd9cdc9a17c805
applebarrientos <apple.barrientos@edu.sait.ca>
2024-09-13 14:56:34 -0600

    week-2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StudentInfo(){
+    return (
+        <header>
+            <h1>Apple Barrientos</h1>            
+            <p><Link href="https://github.com/applebarrientos">Link to my GitHub</Link></p>
+        </header>
+    );
+}
