592aab04b51aa2746eb18ad44adbe54cd65a6712
applebarrientos <apple.barrientos@edu.sait.ca>
2024-09-25 19:09:15 -0600

    modified some styles

--- app/page.js
+++ app/page.js
@@ -2,15 +2,23 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const weekDemos = [2, 3];
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <li>
-        <Link href="/week-2">Week-2</Link>
-      </li>
-      <li>
-        <Link href="/week-3">Week-3</Link>
-      </li>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
+        <h1 className="text-4xl font-bold mb-5">
+          CPRG 306: Web Development 2 - Assignments
+        </h1>
+        <div className="text-lg">
+          <ul>
+            {weekDemos.map((week) => (
+              <li key={week} className="hover:text-red-400">
+                <Link href={`/week-${week}`}>Week {week} Demo</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </main>
   );
 }
